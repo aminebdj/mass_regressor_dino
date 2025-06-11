e66c265b81969bd58798edca9d545db714ab0c33
aminebdj <mohamed.boudjoghra@mbzuai.ac.ae>
2025-06-11 15:49:44 +0200

    updated

--- submit_job.sh
+++ submit_job.sh
@@ -11,4 +11,4 @@
 #SBATCH --exclude=ikarus
 
 # Run your script
-python train.py --num_epochs 1000 --experiment_name clip_ft_maple --batch_size 128 --data_path /cluster/umoja/aminebdj/datasets/ABO/abo-benchmark-material --gt_path /cluster/umoja/aminebdj/datasets/ABO/abo_500/filtered_product_weights.json --val_path /cluster/umoja/aminebdj/datasets/ABO/abo_500/scenes
+python train.py --num_epochs 1000 --experiment_name clip_ft_maple --overfit False --batch_size 128 --data_path /cluster/umoja/aminebdj/datasets/ABO/abo-benchmark-material --gt_path /cluster/umoja/aminebdj/datasets/ABO/abo_500/filtered_product_weights.json --val_path /cluster/umoja/aminebdj/datasets/ABO/abo_500/scenes
