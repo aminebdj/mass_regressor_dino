a2a08e3f815acb15eef6c3dc8479e41a04ef6d2d
aminebdj <mohamed.boudjoghra@mbzuai.ac.ae>
2025-06-12 13:14:59 +0200

    updated

--- trainer.py
+++ trainer.py
@@ -149,8 +149,8 @@ def train(data_path,gt_path,val_path,device='cuda', batch_size=8, save_best_mode
     train_dataset = ABO_DATASET(split='train', overfit=overfit, path_to_dataset=data_path, val_path=val_path, path_to_annotations=gt_path)
     val_dataset = ABO_DATASET(split='val', overfit=overfit, path_to_dataset=data_path, val_path=val_path, path_to_annotations=gt_path)
     # train_dataset[0]
-    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, num_workers=num_workers-1, shuffle=True)
-    val_dataloader = DataLoader(val_dataset, batch_size=1, num_workers=num_workers-1, shuffle=False)
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, num_workers=22, shuffle=True)
+    val_dataloader = DataLoader(val_dataset, batch_size=1, num_workers=22, shuffle=False)
 
     # best_val_loss = float('inf')
 
