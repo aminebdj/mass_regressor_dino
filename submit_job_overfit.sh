ba8d463dd518624a1d703032188c90345e804283
aminebdj <mohamed.boudjoghra@mbzuai.ac.ae>
2025-06-15 15:50:05 +0200

    updated

--- submit_job_overfit.sh
+++ submit_job_overfit.sh
@@ -3,7 +3,7 @@
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mem=64gb
 #SBATCH --gres=gpu:1
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=16
 #SBATCH --constraint="a100|rtx_a6000"
 #SBATCH --time=4-00:00:00
 #SBATCH --output=./slurm/overfit/of_maple_classifier_bb_%j.log
