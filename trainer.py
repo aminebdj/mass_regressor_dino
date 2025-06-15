acb5146b026c4707961d48e9f16cc682e8129f50
aminebdj <mohamed.boudjoghra@mbzuai.ac.ae>
2025-06-15 16:25:18 +0200

    updated

--- trainer.py
+++ trainer.py
@@ -202,7 +202,7 @@ def train(data_path,gt_path,val_path, path_to_3d_samples,device='cuda', batch_si
             tragets_ext = targets
             # tragets_ext = targets.repeat_interleave(images.shape[1], dim=0)
             # loss = soft_cross_entropy(logits, tragets_ext.float())
-            loss = (logits[:, 0]- tragets_ext[:, 0].float()).abs().mean()
+            loss = 0.001*(logits[:, 0]- tragets_ext[:, 0].float()).abs().mean()
 
             maple_trainer.optim.zero_grad()
             loss.backward()
