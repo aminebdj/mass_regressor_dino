e686a7a891f45776051949326b586e005e5f9ff1
aminebdj <mohamed.boudjoghra@mbzuai.ac.ae>
2025-06-15 16:19:27 +0200

    updated

--- trainer.py
+++ trainer.py
@@ -139,9 +139,9 @@ def evaluate(maple_trainer, dataloader, device, num_images=3):
             sparse_input = ME.SparseTensor(coordinates=voxels.to(device), features=features.to(device))
             pred_logits = maple_trainer.model(batch, sparse_input)
             # preds = pred_logits
-            preds = F.softmax(pred_logits, dim=1)  # still on GPU
-            all_preds.append(preds)
-            preds = torch.cat(all_preds)
+            # preds = F.softmax(pred_logits, dim=1)  # still on GPU
+            # all_preds.append(preds)
+            # preds = torch.cat(all_preds)
             # Extend targets to match number of images (move to CPU as well)
             # tragets_ext = targets.repeat_interleave(images.shape[1], dim=0)
             tragets_ext = targets
@@ -149,7 +149,7 @@ def evaluate(maple_trainer, dataloader, device, num_images=3):
             # Compute loss using mass weighting
             min_mass = dataloader.dataset.min_w
             max_mass = dataloader.dataset.max_w
-            pred_mass = preds[:, 0]*(max_mass-min_mass) + min_mass
+            pred_mass = pred_logits[:, 0]*(max_mass-min_mass) + min_mass
             target_mass = tragets_ext[:, 0]*(max_mass-min_mass) + min_mass
 
             loss = (pred_mass - target_mass).abs().sum()
@@ -201,8 +201,8 @@ def train(data_path,gt_path,val_path, path_to_3d_samples,device='cuda', batch_si
             logits = maple_trainer.model(images, sparse_input)
             tragets_ext = targets
             # tragets_ext = targets.repeat_interleave(images.shape[1], dim=0)
-            loss = 10*soft_cross_entropy(logits, tragets_ext.float())+0.1*(logits[:, 0]- tragets_ext[:, 0].float()).abs().mean()
-            # loss = (logits[:, 0]- tragets_ext[:, 0].float()).abs().mean()
+            # loss = soft_cross_entropy(logits, tragets_ext.float())
+            loss = (logits[:, 0]- tragets_ext[:, 0].float()).abs().mean()
 
             maple_trainer.optim.zero_grad()
             loss.backward()
