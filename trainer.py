e72bd4ea20453a07ad2517ece2fb4d62f4616d94
aminebdj <mohamed.boudjoghra@mbzuai.ac.ae>
2025-06-15 18:40:20 +0200

    update

--- trainer.py
+++ trainer.py
@@ -151,10 +151,11 @@ def evaluate(maple_trainer, dataloader, device, num_images=3):
             
 
             # Compute loss using mass weighting
-            min_mass = dataloader.dataset.min_w
-            max_mass = dataloader.dataset.max_w
-            print(preds.argmax(dim=-1).cpu().numpy().shape)
-            print(mass_mapping.shape)
+            # min_mass = dataloader.dataset.min_w
+            # max_mass = dataloader.dataset.max_w
+            # print(preds.argmax(dim=-1).cpu().numpy().shape)
+            print(preds.shape)
+            print(preds.argmax(dim=-1).cpu().numpy())
             exit()
             pred_mass = torch.cat([mass_mapping[p_idx] for p_idx in preds.argmax(dim=-1).cpu().numpy()])
             
